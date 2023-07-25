17ff963316631c95252a1164bbd48ec50dabcbe0
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-24 17:28:39 -0700

    commit

--- app/src/main/java/com/kotlinprojects/bookscollection/MainActivity.kt
+++ app/src/main/java/com/kotlinprojects/bookscollection/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
+
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kotlinprojects.bookscollection.ui.theme.BooksCollectionTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +27,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    NavigationAppHost(navController = navController)
+
                 }
             }
         }
     }
 }
 
+@Composable
+fun NavigationAppHost (navController: NavHostController){
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home"){ HomeScreen()}
+        composable("feed"){ FeedScreen() }
+        composable("profile"){ ProfileScreen()}
+    }
+
+}
